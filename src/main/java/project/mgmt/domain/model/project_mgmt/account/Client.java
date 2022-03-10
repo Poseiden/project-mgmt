ecc3cab3610ac3c2e8c83d32a13d602a4324851b
hbzhu <hbzhu@thoughtworks.com>
2022-03-10 17:48:18 +0800

    [refact] rename account to client.

--- src/main/java/project/mgmt/domain/model/project_mgmt/account/Client.java
+++ src/main/java/project/mgmt/domain/model/project_mgmt/account/Client.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Account {
+public class Client {
     private String id;
     private String manager;
     private List<String> contractIdList;
